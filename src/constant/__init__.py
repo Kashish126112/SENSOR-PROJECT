7c3959b7d24f28e3035b200742f483a049b82bd1
Kashish126112 <kashish126112@gmail.com>
2024-10-06 08:56:53 +0530

    added more files

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -7,7 +7,7 @@ MONGO_COLLECTION_NAME = "waferfault"
 
 
 TARGET_COLUMN = "quality"
-MONGO_DB_URL="mongodb+srv://imran:TdPLW9Ad0OzpSSD2@cluster0.fv0lm61.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_DB_URL="mongodb+srv://kashish126112:RUCrO3BbYFonNUaM@kashish.s1o7z.mongodb.net/?retryWrites=true&w=majority&appName=Kashish"
 
 
 MODEL_FILE_NAME = "model"
